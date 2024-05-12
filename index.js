e5cd40cfc68837ef1e746b315e6138cb76678fae
Rupesh Mohanty <rupeshmohanty67@gmail.com>
2024-05-12 19:12:45 +0530

    New code for ip address added

---
+++ index.js
@@ -0,0 +1,42 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function (req, res) {
+    var ip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
+    var language = "en-US,en;q=0.9,en-IN;q=0.8"
+    var software = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0"
+
+    res.json({
+        ipaddress: ip,
+        language: language,
+        software: software
+    })
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
